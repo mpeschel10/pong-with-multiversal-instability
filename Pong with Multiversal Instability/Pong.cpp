e6506606d55aa031bc919fc35ae91b1228196bd5
Zachnew01 <125604866+Zachnew01@users.noreply.github.com>
2023-11-07 14:18:53 -0500

    Title Screen & Major Bug Fix
    Implemented a temporary Title Screen with a button to access the normal Player Vs. Player game mode. Modified the window width and height values for consistency/intended functionality.
    - AI still needs to be implemented.

--- Pong with Multiversal Instability/Pong.cpp
+++ Pong with Multiversal Instability/Pong.cpp
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string>
+//#include "MenuScreens.cpp" // Originally a separate file for storing the title screen functions. Deprecated because of complexity (for now)
 
 #define PI 3.14159265
 
 using namespace std;
 
-int windowHeight;
-int windowWidth;
+const int windowHeight = 630;
+const int windowWidth = 1000;
 
 // Paddle 1
 float p1x1;
@@ -30,7 +31,7 @@ float ballX;
 float ballY;
 float ballSpeedX;
 float ballSpeedY;
-const float ballSpeed = 0.10;
+const float ballSpeed = 0.08;
 
 // Scoring
 int player1Score = 0;
@@ -38,12 +39,24 @@ int player2Score = 0;
 
 bool isPaused = false;
 
+// Title Screen
+string title;
+int titleLen;
+string PVP;
+string PVE;
+
+// Function Initialization
+void titleInit();
+void titleDisplay();
+void titleMouse(int button, int state, int x, int y);
+void titleKeyboard(unsigned char key, int x, int y);
+
 void renderText(const string& text, float x, float y) {
     // Professor Reza's Render Text Function
     glMatrixMode(GL_PROJECTION);
     glPushMatrix();
     glLoadIdentity();
-    gluOrtho2D(0, 1000, 0, 650); // Modified for the window size of our application
+    gluOrtho2D(0, windowWidth, 0, windowHeight); // Modified for the window size of our application
 
     glMatrixMode(GL_MODELVIEW);
     glPushMatrix();
@@ -68,22 +81,22 @@ void display() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     glColor3f(0.5f, 0.5f, 0.5f);
-    glRectf((windowWidth / 2.0) - 5, ((windowHeight - 50) / 2.0) - 1, (windowWidth / 2.0) + 5, ((windowHeight - 50) / 2.0) + 1);
-    glRectf((windowWidth / 2.0) - 1, 0, (windowWidth / 2.0) + 1, windowHeight - 51);
+    glRectf((windowWidth / 2.0) - 5, ((windowHeight - 30) / 2.0) - 1, (windowWidth / 2.0) + 5, ((windowHeight - 30) / 2.0) + 1);
+    glRectf((windowWidth / 2.0) - 1, 0, (windowWidth / 2.0) + 1, windowHeight - 31);
     glColor3f(1.0f, 1.0f, 1.0f);
-    
+
     glBegin(GL_LINES); // Draw Play Area
     
     glVertex2i(1, 1);
     glVertex2i(windowWidth - 1, 1);
 
     glVertex2i(windowWidth - 1, 1);
-    glVertex2i(windowWidth - 1, windowHeight - 1);
+    glVertex2i(windowWidth - 1, windowHeight - 31);
 
-    glVertex2i(windowWidth - 1, windowHeight -1);
-    glVertex2i(1, windowHeight -1);
+    glVertex2i(windowWidth - 1, windowHeight - 31);
+    glVertex2i(1, windowHeight - 31);
 
-    glVertex2i(1, windowHeight - 1);
+    glVertex2i(1, windowHeight - 31);
     glVertex2i(1, 1);
 
     glEnd();
@@ -97,55 +110,36 @@ void display() {
     glRectf(p2x1, p2y1, p2x2, p2y2);
 
     // Ball
-    glRectf(ballX - 5, ballY - 5, ballX + 5, ballY + 5);
+    glRectf(ballX - 4, ballY - 4, ballX + 4, ballY + 4);
 
     // Render Score
     string score = to_string(player1Score) + " | " + to_string(player2Score);
     int scoreLength = to_string(player1Score).length();
-    //renderText(score, (windowWidth / 2), (windowHeight - 50));
-    renderText(score, 491.0 - scoreLength*12.0, 623.0);
-    
+    renderText(score, 491.0 - scoreLength*12.0, 606.0);
+
     glutSwapBuffers();
 }
 
 void reshape(int width, int height) {
-    glutReshapeWindow(1000, 650);
+    glutReshapeWindow(windowWidth, windowHeight);
 }
 
 void reset() {
-    windowWidth = glutGet(GLUT_SCREEN_WIDTH);
-    windowHeight = glutGet(GLUT_SCREEN_HEIGHT);
+
+    cout << windowWidth << " | " << windowHeight << endl;
 
     p1x1 = 10.0;
-    p1y1 = ((windowHeight - 50) / 2.0) + 50.0;
+    p1y1 = ((windowHeight - 30) / 2.0) + 50.0;
     p1x2 = 20.0;
-    p1y2 = ((windowHeight - 50) / 2.0) - 50.0;
+    p1y2 = ((windowHeight - 30) / 2.0) - 50.0;
 
     p2x1 = windowWidth - 10.0;
-    p2y1 = ((windowHeight - 50) / 2.0) + 50.0;
+    p2y1 = ((windowHeight - 30) / 2.0) + 50.0;
     p2x2 = windowWidth - 20.0;
-    p2y2 = ((windowHeight - 50) / 2.0) - 50.0;
+    p2y2 = ((windowHeight - 30) / 2.0) - 50.0;
 
     ballX = windowWidth / 2.0;
-    ballY = (windowHeight - 50) / 2.0;
-
-    /*int rdm = rand() % 4 + 1;
-    if (rdm == 1) {
-    ballSpeedX = -ballSpeed;
-    ballSpeedY = ballSpeed;
-    }
-    else if (rdm == 2) {
-        ballSpeedX = -ballSpeed;
-        ballSpeedY = -ballSpeed;
-    }
-    else if (rdm == 3) {
-        ballSpeedX = ballSpeed;
-        ballSpeedY = ballSpeed;
-    }
-    else {
-        ballSpeedX = ballSpeed;
-        ballSpeedY = -ballSpeed;
-    } */
+    ballY = (windowHeight - 30) / 2.0;
 
     int rdm = rand() % 4 + 1;
     if (rdm == 1) {
@@ -165,19 +159,21 @@ void reset() {
 }
 
 void idle() {
-    if ((ballY + 5 >= windowHeight - 51) || (ballY - 5 <= 1)) {
+
+    if ((ballY + 5 >= windowHeight - 31) || (ballY - 5 <= 1)) {
         ballSpeedY = 0.0 - ballSpeedY;
     }
     else if (ballX <= p1x2 && ballX >= p1x1 && ballY >= p1y2 && ballY <= p1y1) {
         ballX = p1x2;
-        ballSpeedX = 0.0 - ballSpeedX;
+        ballSpeedX = 0 - ballSpeedX;
     }
     else if (ballX >= p2x2 && ballX <= p2x1 && ballY >= p2y2 && ballY <= p2y1) {
         ballX = p2x2;
-        ballSpeedX = 0.0 - ballSpeedX;
+        ballSpeedX = 0 - ballSpeedX;
     }
     else if ((ballX + 5 >= windowWidth - 1)) {
         //cout << "Player 1 wins the game" << endl;
+        player1Score += 1;
         //reset();
         glutIdleFunc(NULL);
         glutPostRedisplay();
@@ -185,6 +181,7 @@ void idle() {
     }
     else if ((ballX - 5 <= 1)) {
         //cout << "Player 2 wins the game" << endl;
+        player2Score += 1;
         //reset();
         glutIdleFunc(NULL);
         glutPostRedisplay();
@@ -199,7 +196,7 @@ void keyboard(unsigned char key, int x, int y) {
     switch (key) {
     
     case 'w': 
-        if (p1y1 <= windowHeight - 60) {
+        if (p1y1 <= windowHeight - 40) {
             p1y1 += paddleSpeed;
             p1y2 += paddleSpeed;
         }
@@ -226,9 +223,17 @@ void keyboard(unsigned char key, int x, int y) {
             glutIdleFunc(NULL);
         }
         break;
-    
+
     case 27:
-        exit(0);
+        //exit(0);
+        player1Score = 0;
+        player2Score = 0;
+        titleInit();
+        glutDisplayFunc(titleDisplay);
+        glutKeyboardFunc(titleKeyboard);
+        glutMouseFunc(titleMouse);
+        glutIdleFunc(NULL);
+        glutSpecialFunc(NULL);
         break;
     }
     glutPostRedisplay();
@@ -238,7 +243,7 @@ void special(int key, int x, int y) {
    
     switch (key) {
     case GLUT_KEY_UP:
-        if (p2y1 <= windowHeight - 60) {
+        if (p2y1 <= windowHeight - 40) {
             p2y1 += paddleSpeed;
             p2y2 += paddleSpeed;
         }
@@ -254,10 +259,9 @@ void special(int key, int x, int y) {
 }
 
 void init() {
-    
-    windowWidth = glutGet(GLUT_SCREEN_WIDTH);
-    windowHeight = glutGet(GLUT_SCREEN_HEIGHT);
-    
+
+    cout << windowWidth << " | " << windowHeight << endl;
+
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(0, windowWidth, 0, windowHeight);
@@ -265,25 +269,96 @@ void init() {
 
     srand(time(0));
     reset();
-    
+
+}
+
+// -- Title Screen Functions --
+
+void titleInit() {
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, windowWidth, 0, windowHeight);
+    glMatrixMode(GL_MODELVIEW);
+
+    title = "PONG WITH MULTIVERSAL INSTABILITY";
+    titleLen = title.length() / 2.0;
+
+    PVP = "Player vs. Player";
+    PVE = "Player vs. AI";
+}
+
+void titleDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Central Guide Line
+//    glColor3f(0.5f, 0.5f, 0.5f);
+//    glRectf((menuWindowWidth / 2.0) - 1, 0, (menuWindowWidth / 2.0) + 1, menuWindowHeight);
+//    glColor3f(1.0f, 1.0f, 1.0f);
+
+    // Title
+    renderText(title, (windowWidth / 2.0) - (titleLen * 15.0), 500.0);
+
+    // Player Vs. Player
+    glColor3f(0.5f, 0.5f, 0.5f);
+    glRectf((windowWidth / 2.0) - 85, 375, (windowWidth / 2.0) + 85, 425);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    renderText(PVP, (windowWidth / 2.0) - 80, 390.0);
+
+
+    // Player Vs. AI
+    glColor3f(0.5f, 0.5f, 0.5f);
+    glRectf((windowWidth / 2.0) - 70, 275, (windowWidth / 2.0) + 70, 325);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    renderText(PVE, (windowWidth / 2.0) - 63, 290.0);
+
+    glutSwapBuffers();
+}
+
+void titleKeyboard(unsigned char key, int x, int y) {
+    switch (key) {
+    case 27:
+        exit(0);
+        break;
+    }
+    glutPostRedisplay();
+}
+
+void titleMouse(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        y = windowHeight - y;
+        cout << "Mouse Click | X: " << x << "| Y: " << y << endl;
+
+        if ((x >= ((windowWidth / 2.0) - 80)) && (x <= ((windowWidth / 2.0) + 80)) && (y >= 375) && (y <= 425)) {
+            init();
+            glutMouseFunc(NULL);
+            glutKeyboardFunc(keyboard);
+            //glutKeyboardUpFunc();
+            glutSpecialFunc(special);
+            glutDisplayFunc(display);
+            glutIdleFunc(idle);
+            glutPostRedisplay();
+        }
+        else if ((x >= ((windowWidth / 2.0) - 70)) && (x <= ((windowWidth / 2.0) + 70)) && (y >= 275) && (y <= 325)) {
+            cout << PVE << endl;
+        }
+    }
 }
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(1000, 650);
-    glutInitWindowPosition(50,50);
+    glutInitWindowSize(windowWidth, windowHeight);
+    glutInitWindowPosition(50, 50);
     glutCreateWindow("Pong with Multiversal Instability");
 
-    init();
+    titleInit();
+    glutDisplayFunc(titleDisplay);
+    glutKeyboardFunc(titleKeyboard);
+    glutMouseFunc(titleMouse);
 
-    glutKeyboardFunc(keyboard);
-    //glutKeyboardUpFunc();
-    glutSpecialFunc(special);
     glutReshapeFunc(reshape);
-    glutDisplayFunc(display);
-    glutIdleFunc(idle);
+
     glutMainLoop();
     return 0;
 }
